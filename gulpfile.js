da66190c981dd82c8ef774f812f3cbbfb1586c18
wujingtao <601595686@qq.com>
2018-01-06 01:55:06 +0800

    更新了编译规则

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,18 @@ const ts = require("gulp-typescript").createProject('tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
 
 //编译TS代码
-gulp.task("compile", function () {
+gulp.task("compileTS", function () {
     return gulp.src('src/**/*.ts')
         .pipe(sourcemaps.init())
         .pipe(ts())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('bin'));
 });
+
+//复制资源文件
+gulp.task("copeFile", function () {
+    return gulp.src('src/**/*.!(ts)')
+        .pipe(gulp.dest('bin'));
+});
+
+gulp.task("compile", ["compileTS", "copeFile"]);
