c2c90142ecc18f8eb5a6a99bd63ec236ad23f717
wujingtao <601595686@qq.com>
2018-03-22 12:43:44 +0800

    更新了readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # stock-data-downloader
 股票数据下载器
 
+### 目前支持下载
+* A股基本面数据
+* A股、港股(沪港通) 日线与后复权日线数据
+* 国内期货、外汇日线数据
+
 ### 环境变量配置
 * `MYSQL_HOST_ADDR` 主机地址 (默认 localhost)
 * `MYSQL_HOST_PORT` 连接端口 (默认 3306)
@@ -9,3 +14,5 @@
 
 ### Docker Image
 > docker pull registry.cn-hangzhou.aliyuncs.com/wujingtao/stock-data-downloader
+
+> [用法参考](./test/remote_debug_config/debug.sh)
--- test/remote_debug_config/debug.sh
+++ test/remote_debug_config/debug.sh
@@ -3,6 +3,15 @@
 # 拉取新镜像
 docker pull registry.cn-hangzhou.aliyuncs.com/wujingtao/stock-data-downloader:latest
 
+# 创建或更新正式上线的容器
+docker rm -f stock-data-downloader; \
+docker run -d --restart=always --name stock-data-downloader \
+-e MYSQL_HOST_ADDR='localhost' \
+-e MYSQL_HOST_PORT='3306' \
+-e MYSQL_USERNAME='root' \
+-e MYSQL_PASSWORD='root' \
+registry.cn-hangzhou.aliyuncs.com/wujingtao/stock-data-downloader:latest
+
 # 创建调试容器
 docker run -it -p 9229:9229 --name test-stock-data-downloader \
 -e MYSQL_HOST_ADDR='localhost' \
@@ -16,12 +25,3 @@ node --inspect-brk=0.0.0.0:9229 .
 
 # 删除调试容器
 docker rm -f test-stock-data-downloader
-
-# 创建或更新正式上线的容器
-docker rm -f stock-data-downloader; \
-docker run -d --restart=always --name stock-data-downloader \
--e MYSQL_HOST_ADDR='localhost' \
--e MYSQL_HOST_PORT='3306' \
--e MYSQL_USERNAME='root' \
--e MYSQL_PASSWORD='root' \
-registry.cn-hangzhou.aliyuncs.com/wujingtao/stock-data-downloader:latest
